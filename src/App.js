ffd04503c088df0cbe5ea8cb7040713883029f82
nguye374 <nguye374@sheridancollege.ca>
2021-10-16 00:23:09 -0400

    Oct 16

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Greeting from './components/Greeting';
+// import Greeting from './components/Greeting';
 import './App.css';
 import Async from './components/Async';
 
 function App() {
   return (
     <div className="App">
-      <Greeting/>
       <Async/>
     </div>
   );
